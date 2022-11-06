02af4d7bd6f46edcfe4cf6303c854651f50b53dd
Jesus Santiago Tovar <jesusjames9373@gmail.com>
2022-11-05 19:18:35 -0500

    Create Button component

--- pages/landing/LadingPage.js
+++ pages/landing/LadingPage.js
@@ -1,8 +1,35 @@
+
 import React from 'react'
+import Button, { Variant } from './../../components/Button/Button';
 
 const LadingPage = () => {
   return (
-    <div><p>LadingPage kupokea </p></div>
+    <div>
+        <p>LadingPage kupokea </p>
+        <Button onClick={(e) => {console.log('click', e)}}>
+            Launch App
+        </Button>
+        <br/>
+        <Button withtBorder={false} block>
+            Launch App
+        </Button>
+        <br/>
+        <Button variant={Variant.secondary}>
+            Launch App
+        </Button>
+        <br/>
+        <Button variant={Variant.secondary} withtBorder={false}>
+            Launch App
+        </Button>
+        <br/>
+        <Button variant={Variant.tertiary}>
+            Launch App
+        </Button>
+        <br/>
+        <Button variant={Variant.tertiary} withtBorder={false}>
+            Launch App
+        </Button>
+    </div>
   )
 }
 
