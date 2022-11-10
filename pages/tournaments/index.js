e9fb49508d7c1716e176a677959aa30341e3f8cb
Alejandro Romeo <tule@UnaComputadora.local>
2022-11-10 15:29:26 -0300

    update Tule Filtros

--- pages/tournaments/index.js
+++ pages/tournaments/index.js
@@ -7,6 +7,7 @@ import CardGradient from '@components/CardGradient/CardGradient';
 import { connectWallet, 
         getCurrentWalletConnected,
         loadCurrentMessage,
+        loadProdesFullyByTule,
     } from '../../utils/interact'
 import Link from 'next/link';
 
@@ -15,6 +16,7 @@ export default function Tournaments() {
     const [walletAddress, setWallet] = useState("");
     const [status, setStatus] = useState("");
     const [allProdes, setAllProdes] = useState([]);
+    const [filters, setFilters] = useState({addressFilter:null, nicknameFilter:null, searchFilter:''}) // Aca guardo los filtros
 
   //called only once
     useEffect(() => {
@@ -62,19 +64,12 @@ export default function Tournaments() {
 ////////     \\\\\\\\\\\\
 
     const getAllProdes = async () => {
-        
-        const prodes = await loadCurrentMessage();
+        const fastprodes = await loadCurrentMessage();
+        setAllProdes(fastprodes); // Esta funcion trae solo el retrieve del Factory
+        const prodes = await loadProdesFullyByTule();
+        setAllProdes(prodes);  // Est afuncion le mete el participant array
 
-        const prodesCleaned = prodes.map(prode => {
-            return {
-                prodeNickname: prode.prodeNickname,
-                prodeAddress: prode.prodeAddress,
-                buyIn: prode.buyIn,
-                hidden: prode.hidden,
-            };
-        });
-
-        setAllProdes(prodesCleaned)
+        return
   };
   
 
@@ -91,6 +86,10 @@ export default function Tournaments() {
           key: "buyin",
           label: "Buy-in",
         },
+        {
+            key: "Players",
+            label: "Players",
+          },
       ];
 
     const columnList = columns.map(item => 
@@ -112,13 +111,14 @@ export default function Tournaments() {
                     )}
                 </Button>
             </Header>
-            <div className='w-full container relative px-8 md:px-28 mx-auto pt-8'>
+            <div className='container relative w-full px-8 pt-8 mx-auto md:px-28'>
                 <CardGradient>
                     <Text tag={'h2'} color={'#64CC98'} fontSize='36px' fontSizeSm={'16px'}>Search tournament</Text>
                         <div> 
                         <form className='flex flex-col flex-grow'>
-                            <div className='flex w-full mt-5 px-5 py-3'>
-                                <input type="text" className='rounded-md flex-grow max-w-xs text-[#262333] focus:outline-none'/>
+                            <div className='flex w-full px-5 py-3 mt-5'>
+                                <input onChange={(inputTxt)=>setFilters({...filters, searchFilter:inputTxt.target.value})} type="text" className='rounded-md flex-grow max-w-xs text-[#262333] focus:outline-none'/>
+                                
                                 <h2>or</h2>
                                 {walletAddress.length > 0 ? (
                                 <Link href="/create_tournament"><Button>Create your own</Button></Link> ) : (
@@ -126,20 +126,35 @@ export default function Tournaments() {
                                 )}
                                 
                             </div>
+                            Esto de aca es l oque habria que poner en un contenedor que flote por debajo del search, mas prolijo
+                            <div>
+                                {allProdes.slice(0).reverse().map((prode, index) => {
+                                    if(filters.searchFilter !==null){
+                                        if(filters.searchFilter!==''&&prode.prodeAddress?.includes(filters.searchFilter)){
+                                            return<p>Address:  {prode.prodeAddress} click-prodeLanding</p>
+                                        }
+                                        if(filters.searchFilter!==''&&prode.prodeNickname?.includes(filters.searchFilter)){
+                                            return<p>Nickname:  {prode.prodeNickname} click-prodeLanding</p>
+                                        }
+                                    }
+                                    
+                                    })}
+                                    
+                                </div>
                         </form>
                     </div>
                     <div>
                         <div>
                             <h2 className='pl-10 mt-5'>List of tourneys</h2>
                         </div>
-                        <div className='flex flex-col md:flex-row gap-5'>
-                            <button className='bg-[#333647] text-white'  type="submit">Public tourneys</button>
-                            <button className='bg-[#333647] text-white' type="submit">My tourneys</button>
-                            <button className='bg-[#333647] text-white' type="submit">Created by me</button>
+                        <div className='flex flex-col gap-5 md:flex-row'> 
+                            <button className={filters.addressFilter==null?'bg-[#d82fa5] text-white':'bg-[#333647] text-white'}  type="submit">Public tourneys</button> 
+                            <button className={filters.addressFilter!==null?'bg-[#d82fa5] text-white':'bg-[#333647] text-white'}  onClick={filters.addressFilter==null? ()=> setFilters({...filters, addressFilter:walletAddress}) : ()=> setFilters({...filters, addressFilter:null})}>My Tournaments</button>
+                            <button className='bg-[#333647] text-white' onClick={filters.addressFilter==null? ()=> setFilters({...filters, addressFilter:"0x130f5E56220c218953824D2997C4870961CBdD24"}) : ()=> setFilters({...filters, addressFilter:null})}>HardCoded_Tule_address_filter</button>
                         </div>
                     </div>
 
-                    <div className="rounded-t-xl items-center overflow-hidden bg-gradient-to-r from-emerald-50 to-teal-100 p-10">
+                    <div className="items-center p-10 overflow-hidden rounded-t-xl bg-gradient-to-r from-emerald-50 to-teal-100">
 
                         <table className="table-auto">
                             <thead>
@@ -147,11 +162,18 @@ export default function Tournaments() {
                             </thead>
                             <tbody>
                                 {allProdes.slice(0).reverse().map((prode, index) => {
+                                    if(filters.addressFilter !==null){
+                                        let participantAddressList = prode.participantsArray.map(({ beneficiary }) => beneficiary);
+                                        if(!participantAddressList.includes(filters.addressFilter)){
+                                            return
+                                        }
+                                    }
                                     return (
                                         <tr >
-                                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{prode.prodeNickname}</td>
-                                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{prode.prodeAddress}</td>
-                                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{prode.buyIn}</td>
+                                            <td className="px-4 py-2 font-medium border border-emerald-500 text-emerald-600">{prode.prodeNickname}</td>
+                                            <td className="px-4 py-2 font-medium border border-emerald-500 text-emerald-600">{prode.prodeAddress}</td>
+                                            <td className="px-4 py-2 font-medium border border-emerald-500 text-emerald-600">{prode.buyIn}</td>
+                                            <td className="px-4 py-2 font-medium border border-emerald-500 text-emerald-600">{prode.participantsArray?.length}</td>
                                         </tr>
                                         )
                                     })}
