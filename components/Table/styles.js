4984539c905aa3752adfc49db9dcae99e74a6eb5
Carlos Castillo <carlos.castilloperera@gmail.com>
2022-11-14 18:04:36 -0800

    Update styles.js

--- components/Table/styles.js
+++ components/Table/styles.js
@@ -12,16 +12,19 @@ export const TableStyled = styled.table`
             column-width: 20px;
             color: white;
             font-style: normal;
-            font-weight: 400;
-            font-size: 12px;
+            font-weight: 700;
+            font-size: 22px;
             line-height: 29px;
         }
     }
     tbody {
         column-width: 20px;
+        font-size: 22px;
+        line-height: 29px;
         padding: 1rem;
         background: var(--dark-blue-color);
-        border-radius: 1rem;
+        box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 20px;
         border-right: 16px solid var(--dark-blue-color);
         border-left: 16px solid var(--dark-blue-color);
         td {
@@ -29,18 +32,18 @@ export const TableStyled = styled.table`
             font-size: 10px;
         }
         tr:first-child td:first-child {
-            border-top-left-radius: 1rem;
+            border-top-left-radius: 20px;
             background: var(--dark-blue-color);
         }
         tr:first-child td:last-child {
-            border-top-right-radius: 1rem;
+            border-top-right-radius: 20px;
             column-width: auto;
         }
         tr:last-child td:first-child {
-            border-bottom-left-radius: 1rem;
+            border-bottom-left-radius: 20px;
         }
         tr:last-child td:last-child {
-            border-bottom-right-radius: 1rem;
+            border-bottom-right-radius: 20px;
         }
         tr:nth-of-type(2n) {
             background: var(--light-blue-color);
