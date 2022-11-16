c1e5d2ad1efcb0b9644f95d01167ae63a47531b3
haycarlitos <carlos.castilloperera@gmail.com>
2022-11-16 12:47:25 -0800

    setting same index as main branch

--- pages/create_tournament/index.js
+++ pages/create_tournament/index.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState, useContext } from 'react';
 import Button from '@components/Button/Button';
 import Blur from '@components/Blur/Blur';
 import Header from '@components/Header/Header'
-import { createProde, getTransactionReceiptMined } from '@utils/interact';
+import Text from '@components/Text/Text';
+import CardGradient from '@components/CardGradient/CardGradient';
+import { createProde, waitTransaction, getTransactionReceiptMined } from '../../utils/interact';
 import { useRouter } from 'next/router'
+import { UserContext } from '../../context'
 import { useConnect } from '@hooks/useConnect';
+import { useWeb3 } from '@hooks/useWeb3';
 
 
 function CreateTournament() {
@@ -154,7 +158,7 @@ function CreateTournament() {
                     {waiting? <div>
                                     <span>
                                 ✅{" "}
-                                    <a target="_blank" rel="noopener noreferrer" href={`https://gnosisscan.io/${transaction}`}>
+                                    <a target="_blank" rel="noreferrer" href={`https://gnosisscan.io/${transaction}`}>
                                         View the status of your transaction on Gnosisscan!
                                     </a>
                                     <br />
